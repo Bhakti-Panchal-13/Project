0b432753fae8f358e8633caaf3e1b735861ef8b2
Bhakti Panchal <bhaktipanchal13g@gmail.com>
2024-11-15 22:23:08 +0530

    new change

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-function bodyColor(){
-    let redB = Math.floor(Math.random()*255);
-    let greenB = Math.floor(Math.random()*255);
-    let blueB = Math.floor(Math.random()*255);
-    let colorB = `rgb(${redB},${greenB},${blueB})`;
-    return colorB;
-    }
-    let btn = document.querySelector("button");
-btn.addEventListener("click",function(){
-    console.log("generate random color");
-    let colour = bodyColor();
-    let body = document.querySelector("body");
-    body.style.backgroundColor = colour;
-})
+// Add new Features
